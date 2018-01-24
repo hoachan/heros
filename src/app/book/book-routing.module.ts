27314d0d68cdfc5fdd5d7538ff90c5b2a3e2d31a
hoa <hoa@lifestyle-design.jp>
2018-01-24 18:54:03 +0900

    add book function

---
+++ src/app/book/book-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { FindBookPageComponent } from './containers/find-book-page';
+import { ViewBookPageComponent } from './containers/view-book-page';
+import { CollectionPageComponent } from './containers/collection-page';
+
+import { BookExistsGuard } from './guards/book-exists';
+
+
+const routes: Routes = [
+  {
+    path : 'find',
+    component : FindBookPageComponent
+  },
+  {
+    path : ':id',
+    component : ViewBookPageComponent,
+    canActivate : [BookExistsGuard],
+  },
+  {
+    path : '',
+    component : CollectionPageComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BookRoutingModule { }
---
+++ src/app/book/book.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BookRoutingModule } from './book-routing.module';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
+import { ComponentsModule } from './components';
+import { BookEffects } from './effects/book';
+import { CollectionEffects } from './effects/collection';
+import { BookExistsGuard } from './guards/book-exists';
+
+import { FindBookPageComponent } from './containers/find-book-page';
+import { ViewBookPageComponent } from './containers/view-book-page';
+import { SelectedBookPageComponent } from './containers/selected-book-page';
+import { CollectionPageComponent } from './containers/collection-page';
+import { MaterialModule } from '../material';
+
+import { reducers } from './reducers';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BookRoutingModule,
+    MaterialModule,
+    ComponentsModule,
+    /**
+     * StoreModule.forFeature is used for composing state
+     * from feature modules. These modules can be loaded
+     * eagerly or lazily and will be dynamically added to
+     * the existing state.
+     */
+    StoreModule.forFeature('books', reducers),
+
+    /**
+     * Effects.forFeature is used to register effects
+     * from feature modules. Effects can be loaded
+     * eagerly or lazily and will be started immediately.
+     *
+     * All Effects will only be instantiated once regardless of
+     * whether they are registered once or multiple times.
+     */
+    EffectsModule.forFeature([BookEffects, CollectionEffects]),
+  ],
+  declarations: []
+})
+export class BookModule { }
