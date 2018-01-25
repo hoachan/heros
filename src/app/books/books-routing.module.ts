524de8e35b4b8d124069f37362d3d7e161f47aff
hoa <hoa@lifestyle-design.jp>
2018-01-25 16:52:17 +0900

    fix bug attemp to view book detail

--- src/app/books/books-routing.module.ts
+++ src/app/books/books-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
   {
     path : ':id',
     component : ViewBookPageComponent,
-    canActivate : [BookExistsGuard],
+    // canActivate : [BookExistsGuard],
   },
   {
     path : '',
